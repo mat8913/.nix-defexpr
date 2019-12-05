45580ea84229556bc4aea2ffc260a1bced7e6250
Matthew Harm Bekkema <id@mbekkema.name>
2019-12-05 20:47:56 +1100

    gallium-packages: init

--- default.nix
+++ default.nix
@@ -10,4 +10,23 @@ rec {
 
   myanimelist-export = nixpkgs.haskell.lib.doJailbreak (nixpkgs.haskell.lib.markUnbroken nixpkgs.haskellPackages.myanimelist-export);
 
+  gallium-packages = nixpkgs.buildEnv {
+    name = "gallium-packages";
+    paths = [
+      nixpkgs.gitAndTools.git-annex
+      nixpkgs.gitAndTools.gitRemoteGcrypt
+      nixpkgs.gnupg
+      nixpkgs.mpv
+      nixpkgs.powerline-fonts
+      nixpkgs.taskwarrior
+      nixpkgs.brave
+      git-annex-remote-rclone
+      (nixpkgs.pass.withExtensions (exts: [exts.pass-otp]))
+
+      passman-cli
+      myanimelist-export
+    ];
+    extraOutputsToInstall = [ "man" "doc" ];
+  };
+
 }
