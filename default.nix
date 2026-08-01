4264a2867eeedd762a8ca637763ee74c35926c4e
Matthew Harm Bekkema <id@mbekkema.name>
2026-08-01 21:21:51 +1000

    remove proton drive mount

--- default.nix
+++ default.nix
@@ -180,16 +180,6 @@ pkgs = rec {
     WantedBy=default.target
   '';
 
-  proton-mount-service = nixpkgs.writeTextDir "/share/systemd/user/proton-mount.service" ''
-    [Service]
-    ExecStartPre=${nixpkgs.coreutils}/bin/mkdir -p "%t/media/proton"
-    ExecStart=${nixpkgs.rclone}/bin/rclone mount proton: "%t/media/proton" --read-only --dir-cache-time=1s --vfs-cache-mode=full --vfs-cache-max-size=500G
-    Type=simple
-
-    [Install]
-    WantedBy=default.target
-  '';
-
   my-vim = nixpkgs.vim-full.customize {
     vimrcConfig.customRC = ''
       syntax on
@@ -285,7 +275,6 @@ pkgs = rec {
       my-installconf
       my-swaync
       my-syncthing-service
-      proton-mount-service
 
       runsway
       reset-doc-permissions-service
